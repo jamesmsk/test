3cac0b6ff6c815c5a63ded1218bfcd45cf6ab000
James Nguyen <nguyenj6@mskcc.org>
2021-07-29 09:44:34 -0400

    feat: test 2

---
+++ index.js
@@ -0,0 +1,76 @@
+alert("hello");
+{
+  /* <>
+Overview
+
+Accordion
+
+Breadcrumb
+
+Button
+
+Checkbox
+
+Code snippet
+
+Content switcher
+
+Data table
+
+Date picker
+
+Dropdown
+
+File uploader
+
+Form
+
+Inline loading
+
+Link
+
+List
+
+Loading
+
+Modal
+
+Notification
+
+Number input
+
+Overflow menu
+
+Pagination
+
+Progress indicator
+
+Radio button
+
+Search
+
+Select
+
+Slider
+
+Structured list
+
+Tabs
+
+Tag
+
+Text input
+
+Tile
+
+Toggle
+
+Tooltip
+
+UI shell header
+
+UI shell left panel
+
+UI shell right panel
+</>; */
+}
